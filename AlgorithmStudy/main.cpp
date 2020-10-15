6abc985db8317dc079f5229f8e8d878d871a3de3
kjky12 <kjky123@naver.com>
2020-10-15 20:18:52 +0900

    DFS, BFS스터디 중 커밋..

--- AlgorithmStudy/main.cpp
+++ AlgorithmStudy/main.cpp
@@ -15,55 +15,63 @@ using namespace std;
 
 
 /*
-https://programmers.co.kr/learn/courses/30/lessons/43105
+https://programmers.co.kr/learn/courses/30/lessons/43165
 *
+
 문제 설명
-스크린샷 2018-09-14 오후 5.44.19.png
+n개의 음이 아닌 정수가 있습니다. 이 수를 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
 
-위와 같은 삼각형의 꼭대기에서 바닥까지 이어지는 경로 중, 거쳐간 숫자의 합이 가장 큰 경우를 찾아보려고 합니다. 아래 칸으로 이동할 때는 대각선 방향으로 한 칸 오른쪽 또는 왼쪽으로만 이동 가능합니다. 예를 들어 3에서는 그 아래칸의 8 또는 1로만 이동이 가능합니다.
-
-삼각형의 정보가 담긴 배열 triangle이 매개변수로 주어질 때, 거쳐간 숫자의 최댓값을 return 하도록 solution 함수를 완성하세요.
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
 
 제한사항
-삼각형의 높이는 1 이상 500 이하입니다.
-삼각형을 이루고 있는 숫자는 0 이상 9,999 이하의 정수입니다.
+주어지는 숫자의 개수는 2개 이상 20개 이하입니다.
+각 숫자는 1 이상 50 이하인 자연수입니다.
+타겟 넘버는 1 이상 1000 이하인 자연수입니다.
 입출력 예
-triangle	result
-[[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]	30
+numbers	target	return
+[1, 1, 1, 1, 1]	3	5
+입출력 예 설명
 
 */
 
-int solution(vector<vector<int>> triangle) 
+void dfs_recursion(vector<vector<int>>	*pvecPlusMinus, int nDepth)
 {
+
+
+	for (size_t i = 0; i < length; i++)
+	{
+
+	}
+
+}
+
+int solution(vector<int> numbers, int target) 
+{
+	vector<vector<int>>	vecPlusMinus(numbers.size(), vector<int>(2));
+	for (size_t i = 0; i < vecPlusMinus.size(); i++)
+	{
+		vecPlusMinus[i][0] = +1;
+		vecPlusMinus[i][1] = -1;
+	}
+
 	int answer = 0;
 
-	vector<vector<int>> dp(triangle.size());
-	for (size_t i = 0; i < triangle.size(); i++)	//! 마지막은 더해지는 부분이니까 접근할 필욘 없음
-	{
-		if(i == 0)
-			dp[i].resize(1);
-		else
-			dp[i].resize((triangle[i - 1].size()) * 2);
-	}
+	vector<vector<int>>	vecResult(numbers.size() * 2, vector<int>(numbers.size()));
 
-	dp[0][0] = triangle[0][0];
-	
-	for (int i = 0; i < triangle.size() - 1; i++) 
+	for (size_t i = 0; i < 2; i++)
 	{
-		for (int j = 0; j < triangle[i].size(); j++) 
-		{
-			int nIdx = j * 2;
-			dp[i + 1][nIdx] = dp[i][j] + triangle[i + 1][j];
-			dp[i + 1][nIdx + 1] = dp[i][j] + triangle[i + 1][j + 1];
-		}
+		dfs_recursion(&vecPlusMinus, i);
 	}
-
 	
-	answer = *max_element(dp[dp.size() - 1].begin(), dp[dp.size() - 1].end());
+
 
 	return answer;
 }
-
 int main()
 {
 	int n = 5;
@@ -71,33 +79,14 @@ int main()
 
 	vector<int>	vecTemp;
 
-	const int nTmp1 = 1;
-	int naTmp1[nTmp1] = { 7 };
+	const int nTmp1 = 5;
+	int naTmp1[nTmp1] = { 1, 1, 1, 1, 1 };
 	vecTemp.assign(naTmp1, naTmp1 + nTmp1);
 	triangle.push_back(vecTemp);
 
-	const int nTmp2 = 2;
-	int naTmp2[nTmp2] = { 3, 8 };
-	vecTemp.assign(naTmp2, naTmp2 + nTmp2);
-	triangle.push_back(vecTemp);
 
-	const int nTmp3 = 3;
-	int naTmp3[nTmp3] = { 8, 1, 0 };
-	vecTemp.assign(naTmp3, naTmp3 + nTmp3);
-	triangle.push_back(vecTemp);
-
-	const int nTmp4 = 4;
-	int naTmp4[nTmp4] = { 2, 7, 4, 4 };
-	vecTemp.assign(naTmp4, naTmp4 + nTmp4);
-	triangle.push_back(vecTemp);
-
-	const int nTmp5 = 5;
-	int naTmp5[nTmp5] = { 4, 5, 2, 6, 5 };
-	vecTemp.assign(naTmp5, naTmp5 + nTmp5);
-	triangle.push_back(vecTemp);
-
-
-	solution(triangle);
+	int ntarget = 3;
+	solution(vecTemp, ntarget);
 
 
 	return 0;
